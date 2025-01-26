f876166c2f3f0dd9d055b29dc9bc8f5f1bf7c892
Kenzo1130 <tani.kenzo1130@gmail.com>
2025-01-26 12:52:40 +0900

    フキダシ表示の処理

---
+++ src/BukuBuku_2025_GGJ_Unity/Assets/Scripts/RandomSpeech.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+
+public class RandomSpeech : MonoBehaviour
+{
+	[SerializeField]
+	[Header("テキストオブジェクト")]
+	Text speechText;
+
+	[SerializeField]
+	[Header("パネルオブジェクト")]
+	GameObject speechPanel;
+
+	[SerializeField]
+	[Header("セリフたち")]
+	private string[] speeches;
+
+	// 次に振り向くまでの待機時間
+	[SerializeField]
+	[Header("次に振り向くまでの時間")]
+	float nextWaitingTime = 10.0f;
+
+	// タイマー
+	[SerializeField]
+	[Header("制限時間")]
+	private float limitTime = 60;		// 本来は制限時間をゲームマネージャーから取得したい
+
+	void Start()
+    {
+		StartCoroutine(PlayRandomSpeak());
+	}
+
+	public IEnumerator PlayRandomSpeak()
+	{
+		while (limitTime > 0)                                        // 制限時間までループする
+		{
+			yield return null;
+			limitTime -= Time.deltaTime;                            // 経過時間の加算
+			yield return StartCoroutine(NextRandomSpeak());			// 喋る
+		}
+	}
+
+	IEnumerator NextRandomSpeak()
+	{
+		float timer = 0;
+		while (nextWaitingTime > timer)
+		{
+			yield return null;               // 待機
+			timer += Time.deltaTime;                                          // タイマーの加算
+		}
+		speechText.text = speeches.ElementAt(Random.Range(0, speeches.Count()));    // ランダムなテキストを反映
+	}
+}
